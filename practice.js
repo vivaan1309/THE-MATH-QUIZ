333bd574bedc9345e108c1f50c94a64cca17146a
vivaan1309 <73713020+vivaan1309@users.noreply.github.com>
2021-10-31 13:58:53 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,10 @@
+
+//ADD YOUR FIREBASE LINKS
+
+function adduser()
+{
+user_name=document.getElementById("user_name").value;
+firebase.database().ref("/").child(user_name) .update({
+purpose:"adding user"});
+
+}
